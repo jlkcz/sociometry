b2bf4a4ee21c444b5e95e91fd1db2d8ebc8b5361
Jakub Lucký <Jakub@jakublucky.cz>
2013-11-26 09:35:10 +0100

    Support for production/development switch in cli.py

--- cli.py
+++ cli.py
@@ -1,7 +1,9 @@
 #!venv/bin/python
 # -*- coding: utf8 -*-
-
 from sociometry import app
+import os.path, sys
+
+DEBUG = True
 
 class DevelopmentConfig(object):
     DEBUG = True
@@ -10,7 +12,34 @@ class DevelopmentConfig(object):
     TRAP_HTTP_EXCEPTIONS = True
     TRAP_BAD_REQUEST_ERRORS = True
 
-app.config.from_object(DevelopmentConfig())
-app.config["DATABASE"] = "./sociometry.db"
-app.config["ALLOW_B3"] = False
+class ProductionConfig(object):
+    DEBUG = False
+    TESTING = False
+    HOST = "127.0.0.1"
+    TRAP_HTTP_EXCEPTIONS = False
+    TRAP_BAD_REQUEST_ERRORS = False
+
+def find_appdata():
+    APPNAME = "sociometry"
+    if sys.platform == 'darwin':
+        from AppKit import NSSearchPathForDirectoriesInDomains
+        # http://developer.apple.com/DOCUMENTATION/Cocoa/Reference/Foundation/Miscellaneous/Foundation_Functions/Reference/reference.html#//apple_ref/c/func/NSSearchPathForDirectoriesInDomains
+        # NSApplicationSupportDirectory = 14
+        # NSUserDomainMask = 1
+        # True for expanding the tilde into a fully qualified path
+        appdata = os.path.join(NSSearchPathForDirectoriesInDomains(14, 1, True)[0], APPNAME)
+    elif sys.platform == 'win32':
+        appdata = os.path.join(os.environ['APPDATA'], APPNAME)
+    else:
+        appdata = os.path.expanduser(os.path.join("~", "." + APPNAME))
+    return appdata
+
+if DEBUG:
+    app.config.from_object(DevelopmentConfig())
+    app.config["DATABASE"] = "./sociometry.db"
+else:
+    app.config.from_object(ProductionConfig())
+    app.config["DATABASE"] = find_appdata()+"/sociometry.db"
+
+app.config["ALLOW_B3"] = True
 app.run()
